ba850e91a838d6e63d3e21e471d08c697142c2e7
Mateus Lins <mateuslins94@gmail.com>
2018-12-06 15:09:17 -0200

    Dados não normalizados, porém já pré-processados

--- 01_Preprocessing/diabetes_csv.py
+++ 01_Preprocessing/diabetes_csv.py
@@ -19,8 +19,7 @@ data = pd.read_csv('diabetes_dataset.csv')
 # Criando X and y par ao algorítmo de aprendizagem de máquina.\
 print(' - Criando X e y para o algoritmo de aprendizagem a partir do arquivo diabetes_dataset')
 # Caso queira modificar as colunas consideradas basta algera o array a seguir.
-feature_cols = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 
-                'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']
+feature_cols = ['Pregnancies', 'Glucose', 'BloodPressure', 'BMI', 'DiabetesPedigreeFunction', 'Age']
 X = data[feature_cols]
 y = data.Outcome
 
@@ -38,7 +37,7 @@ y_pred = neigh.predict(data_app)
 URL = "https://aydanomachado.com/mlclass/01_Preprocessing.php"
 
 #TODO Substituir pela sua chave aqui
-DEV_KEY = "COLOCAR_SUA_KEY_AQUI"
+DEV_KEY = "Ponte de Safena"
 
 # json para ser enviado para o servidor
 data = {'dev_key':DEV_KEY,
